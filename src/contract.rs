28797b305923b09e70f5a6fcdf244c230450b651
Fahad-Mahmood <fahad492@outlook.com>
2022-07-22 06:15:52 +0500

    feat: create thread and query-thread-by-id functions added

--- src/contract.rs
+++ src/contract.rs
@@ -4,34 +4,30 @@ use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response,
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{QueryMsgResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::state::{Thread, THREAD, REPLY_COUNTER, Reply, REPLIES};
+use crate::msg::{ ExecuteMsg, InstantiateMsg, QueryMsg, QueryReplyResponse, GetThreadByIdResponse};
+use crate::state::{Thread, ADMIN, THREAD, REPLY_COUNTER, threads, THREAD_COUNTER, next_thread_counter, Reply, REPLIES};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:tefi_dagora";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
     _env: Env,
     info: MessageInfo,
-    msg: InstantiateMsg,
+    _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    let thread = Thread {
-        title: String::from(&msg.title),
-        msg: String::from(&msg.msg),
-        author: info.sender.clone(),
-    };
-    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
-    THREAD.save(deps.storage, &thread)?;
+    ADMIN.save(deps.storage, &info.sender.clone())?;
     REPLY_COUNTER.save(deps.storage, &0)?;
+    THREAD_COUNTER.save(deps.storage, &0)?;
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
     Ok(Response::new()
         .add_attribute("method", "instantiate")
-        .add_attribute("author", info.sender)
-        .add_attribute("title",  msg.title)
-        .add_attribute("message", msg.msg))
+        .add_attribute("admin", info.sender)
+    )
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -42,9 +38,28 @@ pub fn execute(
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-        ExecuteMsg::UpdateMessage { msg } => update_message(deps, info, msg),
-        ExecuteMsg::AddReply { msg } => add_reply(deps, info, msg),
+        ExecuteMsg::CreateThread {title, content, category} => create_thread(deps, info, title, content, category),
+        ExecuteMsg::UpdateThreadContent { msg, id: u64 } => update_message(deps, info, msg),
     }
+}
+
+pub fn create_thread(deps: DepsMut, info: MessageInfo, title: String, content: String, category: String) ->Result<Response, ContractError> {
+let thread_id = next_thread_counter(deps.storage)?; 
+    let thread = Thread {
+        id: thread_id,
+        title,
+        content: String::from(&content),
+        category,
+        author: info.sender.clone(),
+    };
+   threads().save(deps.storage, &thread_id.to_be_bytes().to_vec(), &thread)?;
+    Ok(
+        Response::new()
+        .add_attribute("method", "create_thread")
+        .add_attribute("author", info.sender)
+        .add_attribute("message", content)
+    )
+    
 }
 
 pub fn update_message(deps: DepsMut, info: MessageInfo, msg: String) -> Result<Response, ContractError> {
@@ -52,7 +67,7 @@ pub fn update_message(deps: DepsMut, info: MessageInfo, msg: String) -> Result<R
         if info.sender != state.author {
            return Err(ContractError::Unauthorized {});
         }
-        state.msg = String::from(&msg);
+        state.content = String::from(&msg);
         Ok(state)
     })?;
     Ok(
@@ -76,7 +91,7 @@ pub fn add_reply(deps: DepsMut, info: MessageInfo, msg: String) -> Result<Respon
         msg: String::from(&msg),
         author: info.sender.clone(),
     };
-    REPLIES.save(deps.storage, id, &reply)?;
+    REPLIES.save(deps.storage, &id.to_be_bytes(), &reply)?;
     Ok(
         Response::new()
         .add_attribute("method", "add_reply")
@@ -88,27 +103,83 @@ pub fn add_reply(deps: DepsMut, info: MessageInfo, msg: String) -> Result<Respon
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::GetMessage {} => to_binary(&query_message(deps)?),
+        QueryMsg::GetThreadById { id } => to_binary(&query_thread_by_id(deps, id)?),
+        QueryMsg::GetReply { key } => to_binary(&query_reply_by_key(deps, key)?),
+        //QueryMsg::GetReplies { offset, limit } => to_binary(&query_replies(deps, offset, limit)?),
     }
 }
 
-fn query_message(deps: Deps) -> StdResult<QueryMsgResponse> {
-    let message_state = THREAD.load(deps.storage)?;
-    let total_replies = REPLY_COUNTER.load(deps.storage)?;
-    Ok(QueryMsgResponse { title: message_state.title, msg: message_state.msg, author: message_state.author, total_replies})
+fn query_thread_by_id(deps: Deps, id: u64) -> StdResult<GetThreadByIdResponse> {
+    let thread = threads().load(deps.storage, &id.to_be_bytes().to_vec())?;
+    Ok(GetThreadByIdResponse { id:thread.id, title: thread.title, content: thread.content, author: thread.author, category: thread.category})
 }
+
+fn query_reply_by_key(deps: Deps, key: u64) -> StdResult<QueryReplyResponse> {
+    let reply  = REPLIES.load(deps.storage, &key.to_be_bytes())?;
+    Ok(QueryReplyResponse {msg: reply.msg, author: reply.author })
+
+} 
+
+/*fn query_replies(deps: Deps, offset: Option<u64>, limit: Option<u64>) -> StdResult<u64> {
+    let mut min_key: u64;
+    let mut max_key: u64;
+
+    match offset {
+        Some(a) => min_key = a,
+        None => min_key = 0,
+    }
+
+    match limit {
+        Some(a) => max_key = a + min_key,
+        None => max_key = 10 + min_key,
+    }
+
+    let replies: StdResult<Vec<_>> = REPLIES
+        .range(
+            deps.storage,
+            Option::Some(Bound::Exclusive(&min_key.to_le_bytes())),
+            Bound::Inclusive(&max_key.to_be_bytes()),
+            Order::Descending,
+        )
+        .collect()?;
+    Ok(4)
+}*/
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use cosmwasm_std::testing::{mock_dependencies_with_balance, mock_env, mock_info};
     use cosmwasm_std::{coins, from_binary};
+    #[test]
+    fn create_thread() {
+        let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
+        let msg = InstantiateMsg { };
+        let info = mock_info("creator", &coins(2, "token"));
+        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
 
+        let info = mock_info("anyone", &coins(2, "token"));
+        let title = String::from("First Thread");
+        let content = String::from("First Message");
+        let category = String::from("General");
+        let msg = ExecuteMsg::CreateThread { title: title.clone(), content: content.clone(), category: category.clone()};
+        let _res = execute(deps.as_mut(), mock_env(), info, msg);
+
+         // We should query thread response using id
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetThreadById {id: 1}).unwrap();
+        let value: GetThreadByIdResponse = from_binary(&res).unwrap();
+        assert_eq!(1, value.id);
+        assert_eq!(title, value.title);
+        assert_eq!(content, value.content);
+        assert_eq!(category, value.category);
+
+    }
+}
+/* 
     #[test]
     fn proper_initialization() {
         let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
 
-        let msg = InstantiateMsg { title: String::from("Hello World")  ,msg: String::from("Hello New Message") };
+        let msg = InstantiateMsg {};
         let info = mock_info("creator", &coins(1000, "earth"));
 
         // we can just call .unwrap() to assert this was a success
@@ -166,5 +237,10 @@ mod tests {
         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetMessage {}).unwrap();
         let value: QueryMsgResponse = from_binary(&res).unwrap();
         assert_eq!(1, value.total_replies);
-    }
-}
+
+        // reply should be fetched via query_reply_by_key
+        let reply_res = query(deps.as_ref(), mock_env(), QueryMsg::GetReply {key: 1}).unwrap();
+        let reply_value: QueryReplyResponse = from_binary(&reply_res).unwrap();
+        assert_eq!("This thread is awesome!", reply_value.msg);
+    }*/
+
--- src/msg.rs
+++ src/msg.rs
@@ -2,14 +2,12 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use cosmwasm_std::Addr;
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct InstantiateMsg {
-    pub title: String,
-    pub msg: String,
-}
+pub struct InstantiateMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
+    CreateThread {title: String, content: String, category: String},
     UpdateMessage { msg: String},
     AddReply { msg: String },
 }
@@ -17,9 +15,18 @@ pub enum ExecuteMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    GetMessage {},
+    GetThreadById {id: u64},
+    GetReply {key: u64},
+  //  GetReplies { offset: Option<u64>, limit: Option<u64>}
 }
-
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct GetThreadByIdResponse {
+    pub id: u64,
+    pub title: String,
+    pub content: String,
+    pub category: String,
+    pub author: Addr
+}
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct QueryMsgResponse {
@@ -27,4 +34,9 @@ pub struct QueryMsgResponse {
     pub title: String,
     pub author: Addr,
     pub total_replies: u64,
+}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct QueryReplyResponse {
+    pub msg: String,
+    pub author: Addr,
 }
